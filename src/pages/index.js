dac99c4ccdb7f7968603d31268f46fe8de90120c
Asher Best <ashermcbest@gmail.com>
2021-08-24 19:45:04 -0500

    Finished styling landing page. Started styling mobile nav.

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,38 +2,45 @@ import * as React from 'react';
 import { StaticImage, GatsbyImage, getImage } from 'gatsby-plugin-image';
 import { Link } from 'gatsby';
 import { graphql } from 'gatsby';
-import { Bars, LinkedIn, GitHub, Email } from '../components/icons';
+import { Bars, Cross, LinkedIn, GitHub, Email } from '../components/icons';
 import '../styles/index.scss';
 
 const IndexPage = ({ data }) => {
+  const [showNavList, setShowNavList] = React.useState(false);
+
   return (
     <main>
-      <nav className='nav'>
-        <div className='container nav-inner'>
+      <nav>
+        <div>
           <p className='nav-title'>ASHER BEST</p>
-          <div className='nav-bars'>
+          <div className='nav-bars' onClick={() => setShowNavList(!showNavList)}>
             <Bars size={35} color='#feffff' />
           </div>
-          <ul className='nav-list hide'>
-            <li>
-              <Link to='/#about'>About</Link>
-            </li>
-            <li>
-              <Link to='/#projects'>Projects</Link>
-            </li>
-            <li>
-              <Link to='/blog'>Blog</Link>
-            </li>
-            <li>
-              <Link to='/#contact'>Contact</Link>
-            </li>
-          </ul>
+          <div className={`nav-list-container ${showNavList ? 'show' : 'hide'}`}>
+            <div className='nav-close'>
+              <Cross size={35} color='#feffff' />
+            </div>
+            <ul className='nav-list'>
+              <li>
+                <a href='/#about'>About</a>
+              </li>
+              <li>
+                <a href='/#projects'>Projects</a>
+              </li>
+              <li>
+                <Link to='/blog'>Blog</Link>
+              </li>
+              <li>
+                <a href='/#contact'>Contact</a>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
       <section className='hero'>
-        <div className='container'>
-          <h1 className='hero-title'>
-            Hi there, my name is <span className='text-green'>Asher Best</span>
+        <div>
+          <h1 className='section-title'>
+            Hi there, my name is <span>Asher Best</span>
             <br />
             I'm a JAMStack Developer
           </h1>
@@ -43,9 +50,9 @@ const IndexPage = ({ data }) => {
         </div>
       </section>
       <section id='about' className='about'>
-        <div className='container'>
-          <h2 className='about-title'>ABOUT</h2>
-          <div className='columns'>
+        <div>
+          <h2 className='section-title about-title'>ABOUT ME</h2>
+          <div className='about-card'>
             <div className='about-image-wrapper'>
               <StaticImage src='../images/asher_best.jpg' alt='Asher Best' width={350} height={350} />
             </div>
@@ -55,16 +62,16 @@ const IndexPage = ({ data }) => {
                 nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
                 fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
               </p>
-              <a to='#!' className='call-to-action'>
-                Resume
+              <a href='mailto:ashermcbest@gmail.com' className='call-to-action about-cta'>
+                Get In Touch
               </a>
             </div>
           </div>
         </div>
       </section>
-      <section className='projects'>
-        <div className='container'>
-          <h2 className='projects-title'>PROJECTS</h2>
+      <section id='projects'>
+        <div>
+          <h2 className='section-title projects-title'>PROJECTS</h2>
           {data.allContentfulProject.nodes.map(node => (
             <div key={node.id} className='project-card columns'>
               <div className='project-card-content'>
@@ -81,34 +88,36 @@ const IndexPage = ({ data }) => {
           ))}
         </div>
       </section>
-      <section className='background-green'>
-        <div className='text-center'>
-          <h2>CONTACT</h2>
+      <section id='contact' className='contact'>
+        <div>
+          <h2 className='section-title contact-title'>CONTACT</h2>
           <p>Would you like to work with me? Awesome!</p>
-          <a href='mailto:ashermcbest@gmail.com' className='call-to-action cta-white margin-auto'>
+          <a href='mailto:ashermcbest@gmail.com' className='call-to-action contact-cta'>
             Let's Talk
           </a>
         </div>
       </section>
       <footer>
-        <ul className='social-links'>
-          <li>
-            <a href=''>
-              <LinkedIn size={50} color='#feffff' />
-            </a>
-          </li>
-          <li>
-            <a href=''>
-              <GitHub size={50} color='#feffff' />
-            </a>
-          </li>
-          <li>
-            <a href=''>
-              <Email size={50} circleColor='#feffff' iconColor='#17252A' />
-            </a>
-          </li>
-        </ul>
-        <p className='text-center'>© 2021 - Website designed & developed by Asher Best</p>
+        <div>
+          <ul className='footer-social-links'>
+            <li>
+              <a href='https://www.linkedin.com/in/asher-best-16b121191/'>
+                <LinkedIn size={50} color='#feffff' />
+              </a>
+            </li>
+            <li>
+              <a href='https://github.com/asherb20'>
+                <GitHub size={50} color='#feffff' />
+              </a>
+            </li>
+            <li>
+              <a href='mailto:ashermcbest@gmail.com'>
+                <Email size={50} circleColor='#feffff' iconColor='#17252A' />
+              </a>
+            </li>
+          </ul>
+          <p className='footer-copyright'>© 2021 - Website designed & developed by Asher Best</p>
+        </div>
       </footer>
     </main>
   );
